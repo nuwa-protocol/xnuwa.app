476b2df1bea003ea5e347b5f41ef77fe1816850e
Mine77 <haichaozhu77@gmail.com>
2025-07-03 14:59:33 +0800

    feat: add the sources message to final message for persistance

--- src/features/ai-chat/services/handler/index.ts
+++ src/features/ai-chat/services/handler/index.ts
@@ -1,3 +1,4 @@
+import { LanguageModelV1Source } from '@ai-sdk/provider';
 import {
   appendResponseMessages,
   type Message,
@@ -11,9 +12,6 @@ import { systemPrompt } from '../prompts';
 import { llmProvider } from '../providers';
 import { createDocument } from '../tools/create-document';
 import { updateDocument } from '../tools/update-document';
-
-// Default model to use
-const DEFAULT_CHAT_MODEL = 'chat-model';
 
 // Error handling function
 function errorHandler(error: unknown) {
@@ -30,6 +28,28 @@ function errorHandler(error: unknown) {
   }
 
   return JSON.stringify(error);
+}
+
+function appendSourcesToFinalMessages(
+  finalMessages: Message[],
+  messageId: string,
+  sources: LanguageModelV1Source[],
+): Message[] {
+  return finalMessages.map((message) => {
+    if (message.id === messageId) {
+      return {
+        ...message,
+        parts: [
+          ...(message.parts ?? []),
+          ...sources.map((source) => ({
+            type: 'source' as const,
+            source,
+          })),
+        ],
+      };
+    }
+    return message;
+  });
 }
 
 // Handle AI request, entrance of the AI workflow
@@ -70,13 +90,22 @@ const handleAIRequest = async ({
       updateDocument: updateDocument(),
     },
     abortSignal: signal,
-    async onFinish({ response }) {
+    async onFinish({ response, reasoning, sources }) {
       const finalMessages = appendResponseMessages({
         messages: messages,
         responseMessages: response.messages,
       });
 
-      await updateMessages(sessionId, finalMessages);
+      // the appendResponseMessages function above does not append sources to the final messages
+      // so we need to append them manually
+      await updateMessages(
+        sessionId,
+        appendSourcesToFinalMessages(
+          finalMessages,
+          response.messages[0].id,
+          sources,
+        ),
+      );
     },
   });
 
