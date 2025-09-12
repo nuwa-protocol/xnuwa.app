dad7c676ec2af2a306c7ba898463ac9b63130586
Mine77 <haichaozhu77@gmail.com>
2025-09-12 17:41:02 +0800

    fix auth isConnect error

--- src/shared/hooks/use-auth.ts
+++ src/shared/hooks/use-auth.ts
@@ -2,31 +2,18 @@ import {
   type UseIdentityKitOptions,
   useIdentityKit,
 } from '@nuwa-ai/identity-kit-web';
-import { useEffect, useState } from 'react';
 import { cadopConfig } from '../config/cadop';
-import { NuwaIdentityKit } from '../services/identity-kit';
 
 export const useAuth = (options: UseIdentityKitOptions = {}) => {
   const { state, sdk } = useIdentityKit({
     ...cadopConfig,
     ...options,
   });
-  const { isConnected } = NuwaIdentityKit();
-  const [isKitConnected, setIsKitConnected] = useState(false);
-
-  useEffect(() => {
-    const checkKitConnection = async () => {
-      if (await isConnected) {
-        setIsKitConnected(true);
-      }
-    };
-    checkKitConnection();
-  }, [isConnected]);
 
   return {
     did: state.agentDid,
     isConnecting: state.isConnecting,
-    isConnected: isKitConnected,
+    isConnected: state.isConnected,
     isError: state.error,
     isInitializing: sdk === null,
   };
