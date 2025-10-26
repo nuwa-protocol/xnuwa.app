bbb100ec2a64a0f2b371008fa388f9cab1ab84e8
Mine77 <haichaozhu77@gmail.com>
2025-10-26 16:24:23 +0800

    disable cap store

--- src/router.tsx
+++ src/router.tsx
@@ -1,10 +1,9 @@
 import { createBrowserRouter } from 'react-router-dom';
-import AppLayoutSwitch from './layout/app-layout-switch';
 import { AuthGuard } from '@/features/auth/components/auth-guard';
+import AppLayoutSwitch from './layout/app-layout-switch';
 import CapStudioPage from './pages/cap-studio';
 import ChatPage from './pages/chat';
 import ErrorPage from './pages/error';
-import ExplorePage from './pages/explore';
 import { LandingPage } from './pages/landing';
 import OAuthCallbackPage from './pages/oauth-callback';
 import SettingsPage from './pages/settings';
@@ -17,35 +16,43 @@ const router = createBrowserRouter([
     errorElement: <ErrorPage />,
     children: [
       { index: true, element: <LandingPage /> },
-      { path: 'chat', element: (
+      {
+        path: 'chat',
+        element: (
           <AuthGuard>
             <ChatPage />
           </AuthGuard>
-        )
+        ),
       },
-      { path: 'wallet', element: (
+      {
+        path: 'wallet',
+        element: (
           <AuthGuard>
             <WalletPage />
           </AuthGuard>
-        )
+        ),
       },
-      { path: 'settings', element: (
+      {
+        path: 'settings',
+        element: (
           <AuthGuard>
             <SettingsPage />
           </AuthGuard>
-        )
+        ),
       },
-      { path: 'explore/*', element: (
-          <AuthGuard>
-            <ExplorePage />
-          </AuthGuard>
-        )
-      },
-      { path: 'cap-studio/*', element: (
+      // { path: 'explore/*', element: (
+      //     <AuthGuard>
+      //       <ExplorePage />
+      //     </AuthGuard>
+      //   )
+      // },
+      {
+        path: 'cap-studio/*',
+        element: (
           <AuthGuard>
             <CapStudioPage />
           </AuthGuard>
-        )
+        ),
       },
       { path: 'oauth-callback', element: <OAuthCallbackPage /> },
     ],
