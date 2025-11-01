aac343c39a67fa8e3f73874e8920d2300026487f
Mine77 <haichaozhu77@gmail.com>
2025-11-01 23:21:08 +0800

    add analytics

--- src/main.tsx
+++ src/main.tsx
@@ -1,16 +1,17 @@
-import './index.css';
-import { StrictMode } from 'react';
-import { createRoot } from 'react-dom/client';
-import { RouterProvider } from 'react-router-dom';
 import { RemoteMCPManager } from '@/shared/services/global-mcp-manager';
 import { initSentry } from '@/shared/services/sentry';
-import router from './router.tsx';
+import '@fontsource/geist-mono/400.css';
+import '@fontsource/geist-mono/700.css';
 import '@fontsource/geist-sans/400.css';
 import '@fontsource/geist-sans/500.css';
 import '@fontsource/geist-sans/600.css';
 import '@fontsource/geist-sans/700.css';
-import '@fontsource/geist-mono/400.css';
-import '@fontsource/geist-mono/700.css';
+import { Analytics } from "@vercel/analytics/react";
+import { StrictMode } from 'react';
+import { createRoot } from 'react-dom/client';
+import { RouterProvider } from 'react-router-dom';
+import './index.css';
+import router from './router.tsx';
 
 // Initialize Sentry
 initSentry();
@@ -24,6 +25,7 @@ window.addEventListener('beforeunload', () => {
 // biome-ignore lint/style/noNonNullAssertion: Root element is guaranteed to exist
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
+    <Analytics />
     <RouterProvider router={router} />
   </StrictMode>,
 );
